561c555491f15a9342e634dc322bdde1e8a7427d
Shreyam Gupta <shreyamgupta13asia@gmail.com>
2023-06-21 14:39:19 +0530

    Add Static

---
+++ static/js/cart.js
@@ -0,0 +1,68 @@
+// "Add to Cart" button collection 
+var updateBtns = document.getElementsByClassName('update-cart')
+
+// iterating through each button and adding event listner
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('product_id: ', productId, 'Action: ', action)
+        
+        console.log('USER: ', user)
+        if (user === 'AnonymousUser') {
+            addCookieItem(productId, action)
+        } else {
+            updateUserId(productId, action)
+        }
+    })
+}
+
+function addCookieItem(productId, action){
+    console.log("Not logged in..")
+    
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity': 1}
+        }
+        else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+    
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+        
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Remove item')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart: ', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+    
+}
+
+
+function updateUserId(productId, action){
+    console.log('User logged in, sending data...')
+    var url = '/update_item/'
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action': action})
+    })
+    
+    .then((response) =>{
+        return response.json()
+    })
+    
+    .then((data) =>{
+        console.log('data: ', data)
+        location.reload()
+    })
+    
+}
